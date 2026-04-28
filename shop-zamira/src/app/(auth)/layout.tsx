fda20f4409c6f82e7ebb3648b081370356e53fea
zahra <z.godarzi75@gmail.com>
2026-04-28 23:25:47 +0330

    feat : add layout auth

--- shop-zamira/src/app/(auth)/layout.tsx
+++ shop-zamira/src/app/(auth)/layout.tsx
@@ -1,11 +1,30 @@
+"use client";
+
+import authImage from "@/assets/image/img-layout-auth.png"; // عکس ساعت را اینجا قرار بده
+import Image from "next/image";
+
 export default function AuthLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
 	return (
-		<div className="min-h-screen flex items-center justify-center bg-gray-50">
-			{children}
+		<div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+			<div className="flex items-center justify-center px-6 py-12">
+				<div className="w-full max-w-md">{children}</div>
+			</div>
+
+			<div className="hidden lg:flex relative">
+				<Image
+					src={authImage}
+					alt="Watch image"
+					fill
+					className="object-cover"
+					priority
+				/>
+
+				<div className="absolute inset-0 bg-black/40" />
+			</div>
 		</div>
 	);
 }
