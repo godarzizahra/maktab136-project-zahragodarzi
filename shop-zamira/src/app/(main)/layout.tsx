06927a4e438fd4f7706b9dd8b6f59c759830613a
zahra <z.godarzi75@gmail.com>
2026-04-28 17:28:23 +0330

    fixed -footer

--- shop-zamira/src/app/(main)/layout.tsx
+++ shop-zamira/src/app/(main)/layout.tsx
@@ -1,5 +1,5 @@
-import Header from "@/layout/header/header";
 import Footer from "@/layout/footer/footer";
+import Header from "@/layout/header/header";
 
 export default function MainLayout({
 	children,
@@ -7,7 +7,7 @@ export default function MainLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<div className="min-h-full flex flex-col">
+		<div className="min-h-screen flex flex-col">
 			<Header />
 			<main className="flex-1">{children}</main>
 			<Footer />
