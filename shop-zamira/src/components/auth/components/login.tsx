8f96ece0c0d5ebdee84aa6ecbc32178e6ecb9f9d
zahra <z.godarzi75@gmail.com>
2026-04-28 13:46:46 +0330

    feat(login): add login form UI

--- shop-zamira/src/components/auth/components/login.tsx
+++ shop-zamira/src/components/auth/components/login.tsx
@@ -1,21 +1,47 @@
+"use client";
+
+import { ArrowLeft, Eye, EyeOff } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+
 export default function Login() {
+	const [showPassword, setShowPassword] = useState(false);
 	return (
 		<div className="min-h-screen flex items-center justify-center ">
-			<div className="w-full max-w-md shadow-lg rounded-2xl p-8">
+			<div className="w-full max-w-md shadow-2xl rounded-2xl p-8">
+				<Link
+					href="/"
+					className="absolute top-35 left-135 text-gray-500 hover:text-black transition"
+				>
+					<ArrowLeft size={22} />
+				</Link>
+
 				<h1 className="text-2xl font-bold text-center mb-6">
 					ایجاد حساب کاربری{" "}
 				</h1>
-				<form className="space-y-4">
+
+				<form className=" w-full space-y-4 p-7">
 					<input
 						type="email"
-						className="w-full border rounded-md px-3 py-2"
+						className=" border rounded-md px-3 py-2 focus:ring-2 focus:ring-black/20 outline-none"
 						placeholder="ایمیل "
 					/>
-					<input
-						type="password"
-						className="w-full border rounded-md px-3 py-2"
-						placeholder="رمز عبور"
-					/>
+					<div className="relative">
+						<input
+							type={showPassword ? "text" : "password"}
+							className=" border rounded-md px-3 py-2 focus:ring-2 focus:ring-black/20 outline-none"
+							placeholder="رمز عبور"
+						/>
+
+						<button
+							type="button"
+							onClick={() => setShowPassword(!showPassword)}
+							className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-black transition"
+						>
+							{showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+						</button>
+					</div>
+
 					<button
 						type="submit"
 						className="w-full bg-black text-white py-2 rounded-md"
