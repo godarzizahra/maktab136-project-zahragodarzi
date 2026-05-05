5aba3e848907226884014c250030472ce9e3cc86
zahra <z.godarzi75@gmail.com>
2026-05-05 15:29:39 +0330

    feat(adminLogin): add schema

--- shop-zamira/src/components/admin/components/auth/loginAdmin.tsx
+++ shop-zamira/src/components/admin/components/auth/loginAdmin.tsx
@@ -1,27 +1,30 @@
 "use client";
+import {
+	loginSchema,
+	LoginSchemaType,
+} from "@/components/main/auth/schemas/login.schema";
 import AuthForm from "@/components/shared/authForm";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
-type AdminLoginForm = {
-	username: string;
-	password: string;
-};
 export default function LoginAdmin() {
 	const {
 		register,
 		handleSubmit,
 		formState: { errors },
-	} = useForm<AdminLoginForm>();
+	} = useForm<LoginSchemaType>({
+		resolver: zodResolver(loginSchema),
+	});
 
-	const onSubmit = (data: AdminLoginForm) => {
-		console.log(data);
+	const onSubmit = (data: LoginSchemaType) => {
+		console.log("Admin Login:", data);
 	};
 	return (
 		<AuthForm
 			title="ورود ادمین"
 			buttonText="ورود"
 			fields={[
-				{ name: "username", placeholder: "نام کاربری" },
+				{ name: "email", type: "email", placeholder: "ایمیل" },
 				{ name: "password", placeholder: "رمز عبور" },
 			]}
 			register={register}
