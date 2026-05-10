25daeb59acd22f41769bd026382045c39ee8e056
zahra <z.godarzi75@gmail.com>
2026-05-10 18:18:28 +0330

    refactor(brands): move brand list data outside component

--- shop-zamira/src/components/main/store/home/components/BrandsSection.tsx
+++ shop-zamira/src/components/main/store/home/components/BrandsSection.tsx
@@ -4,30 +4,7 @@ import Image from "next/image";
 import "swiper/css";
 import { Autoplay } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
-
-import Obaku from "@/assets/image/brand-obaku.png";
-import casio from "@/assets/image/casio.png";
-import Citizen from "@/assets/image/citizen.png";
-import Esprit from "@/assets/image/esprit.png";
-import Jaguar from "@/assets/image/jaguar.png";
-import Lacoste from "@/assets/image/lacoste.png";
-import Polo from "@/assets/image/polo.jpg";
-import Seiko from "@/assets/image/seiko.png";
-import Tissot from "@/assets/image/tissot.png";
-import Versace from "@/assets/image/versace.png";
-
-const brands = [
-	{ name: "Casio", logo: casio },
-	{ name: "Citizen", logo: Citizen },
-	{ name: "Esprit", logo: Esprit },
-	{ name: "Jaguar", logo: Jaguar },
-	{ name: "Lacoste", logo: Lacoste },
-	{ name: "Obaku", logo: Obaku },
-	{ name: "Polo", logo: Polo },
-	{ name: "Seiko", logo: Seiko },
-	{ name: "Tissot", logo: Tissot },
-	{ name: "Versace", logo: Versace },
-];
+import { brands } from "../constants/brandsData";
 
 export default function BrandsSection() {
 	return (
