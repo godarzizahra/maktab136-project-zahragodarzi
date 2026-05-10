fd0564166a19a2d259693c45786f0a1cad690be0
zahra <z.godarzi75@gmail.com>
2026-05-10 10:11:03 +0330

    feat(slider): add desktop navigation buttons for product carousel

--- shop-zamira/src/components/main/store/home/components/bestSallerSection.tsx
+++ shop-zamira/src/components/main/store/home/components/bestSallerSection.tsx
@@ -2,6 +2,7 @@
 
 import { useProducts } from "@/hooks/useProducts";
 import { Product } from "@/types/ProductsType";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 import { useRef } from "react";
 import ProductCard from "../../products/components/productscard";
 
@@ -13,19 +14,44 @@ export default function BestSellerSection() {
 
 	if (loading) return null;
 
+	const scroll = (direction: "left" | "right") => {
+		if (!scrollRef.current) return;
+
+		const amount = 400;
+
+		scrollRef.current.scrollBy({
+			left: direction === "left" ? -amount : amount,
+			behavior: "smooth",
+		});
+	};
+
 	return (
-		<section className="py-12">
+		<section className="py-12 relative">
 			<div className="container mx-auto px-4 mb-8">
 				<h2 className="text-2xl md:text-3xl font-bold text-center">
 					پرفروش‌ترین ساعت‌ها
 				</h2>
 			</div>
 
+			<button
+				onClick={() => scroll("left")}
+				className="hidden lg:flex absolute left-0 top-1/2 -translate-y-1/2 z-10 h-10 w-10 items-center justify-center rounded-full bg-white shadow-md hover:bg-gray-100"
+			>
+				<ArrowLeft />
+			</button>
+
+			<button
+				onClick={() => scroll("right")}
+				className="hidden lg:flex absolute right-0 top-1/2 -translate-y-1/2 z-10 h-10 w-10 items-center justify-center rounded-full bg-white shadow-md hover:bg-gray-100"
+			>
+				<ArrowRight />
+			</button>
+
 			<div
 				ref={scrollRef}
 				className="overflow-x-auto scrollbar-hide px-4 pb-2 scroll-smooth"
 			>
-				<div className="flex gap-4 min-w-[1400px]  snap-x snap-mandatory">
+				<div className="flex gap-4 min-w-[1400px] snap-x snap-mandatory">
 					{newestProducts.map((item: Product) => (
 						<div
 							key={item._id}
