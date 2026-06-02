2dba39f4b55b9515b501eb78a53986e34f021ebf
zahra <z.godarzi75@gmail.com>
2026-06-02 17:06:55 +0330

    feat(cart): add max quantity limit to quantity control

--- shop-zamira/src/components/main/store/cart/components/quantityControl.tsx
+++ shop-zamira/src/components/main/store/cart/components/quantityControl.tsx
@@ -3,22 +3,25 @@ import { Trash2 } from "lucide-react";
 
 type Props = {
 	quantity: number;
+	max?: number;
 	onIncrease?: () => void;
 	onDecrease?: () => void;
 };
 
 export default function QuantityControl({
 	quantity,
+	max = 3,
 	onIncrease,
 	onDecrease,
 }: Props) {
 	const isRemovable = quantity <= 1;
+	const isMaxReached = quantity >= max;
 	return (
 		<div className="inline-flex items-center border border-gray-200 rounded-3xl overflow-hidden">
 			<button
 				onClick={onIncrease}
-				className="w-10 h-10 flex items-center justify-center cursor-pointer
-				"
+				disabled={isMaxReached}
+				className="flex h-10 w-10 cursor-pointer items-center justify-center disabled:cursor-not-allowed disabled:opacity-50"
 				type="button"
 			>
 				+
